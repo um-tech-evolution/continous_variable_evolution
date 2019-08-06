f63e131a6ab9eba6480fb6b36badaed13e1a5987
Alden Wright <github@fastmail.us>
2019-08-06 09:11:21 -0600

    Added entropy examples to run_tests.sh.

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Bash scrpt to run basic tests
 cd src
+julia run.jl examples/ent_example1 1
+julia run.jl examples/ent_example2 1
+julia run.jl examples/ent_example3 1
+julia -p 4 run.jl examples/ent_example4 1
+julia run.jl examples/ent_example5 1
 julia run.jl examples/example1 1
 julia run.jl examples/example2 1
 julia run.jl examples/example3 1
